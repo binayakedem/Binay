d661cc50c6b6b6f2c824603c433bfd576fbc0841
binayakedem <binayakedem@gmail.com>
2024-11-23 10:47:27 +0545

    about change

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -15,10 +15,10 @@ const About = () => {
     };
   return (
     <>
-     <div className='h-96  w-full' data-aos="flip-left"
+     <div className='h-60  w-full' data-aos="flip-left"
      data-aos-easing="ease-out-cubic"
      data-aos-duration="2000">
-        <img className='h-96 w-full object-cover' src={banner} alt="banner" />
+        <img className='h-60 w-full object-cover' src={banner} alt="banner" />
         <h1 className='font-bold text-2xl'>About Section</h1>
     </div>
     <div className='bg-dark-50 text-light-gray lg:px-32 md:px-20 px-2'>
