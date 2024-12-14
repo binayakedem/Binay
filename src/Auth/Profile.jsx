0d481cea1a99062a643fbf9a405eba1aa5b369f8
binayakedem <binayakedem@gmail.com>
2024-12-14 19:39:47 +0545

    Profile change

--- src/Auth/Profile.jsx
+++ src/Auth/Profile.jsx
@@ -7,7 +7,8 @@ import { ref, onValue, remove, update } from 'firebase/database';
 import Message from "./Message";
 import Routine from "./Routine";
 import Progress from "./Progress";
-
+import profile from "../assets/logo.jpg";
+import cover from "../assets/image2.png";
 const Profile = () => {
     const [user, setUser] = useState(null);
     const navigate = useNavigate();
@@ -71,13 +72,13 @@ console.log(e)
         <div>
             <div className='lg:px-32 md:px-20 px-2 '>
                 <div className="w-full h-[20vh] lg:h-[70vh] overflow-hidden">
-                    <img className="object-cover h-full w-full rounded-b-2xl" src="https://scontent.fktm8-1.fna.fbcdn.net/v/t1.6435-9/130572423_398414568145391_6120544987997921810_n.jpg?stp=dst-jpg_s960x960&_nc_cat=103&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=VxdWbQ6YkHoQ7kNvgHZRa9q&_nc_zt=23&_nc_ht=scontent.fktm8-1.fna&_nc_gid=AwzbWMcv8eslJ7jkWzns-Eo&oh=00_AYDVAQABq0I--jWzHFObv5mu4XL-ucrZP2WrzYRdL_cxOA&oe=674C0335" alt="background" />
+                    <img className="object-cover h-full w-full rounded-b-2xl" src={cover}/>
                 </div>
                 <div className="grid grid-cols-1 lg:grid-cols-3 justify-items-center items-center gap-4">
                     <div className=" h-40  w-40 md:h-60 md:w-60 overflow-hidden flex justify-center items-center relative mt-[-80px] bg-white rounded-full p-1">
                         <img
                             className="h-full w-full object-cover rounded-full"
-                            src="https://scontent.fktm8-1.fna.fbcdn.net/v/t39.30808-6/278174597_692617362058442_5923177136769062776_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=suHAtPhXw7wQ7kNvgGRsJKm&_nc_zt=23&_nc_ht=scontent.fktm8-1.fna&_nc_gid=ANpCqkM7Aihfy39YfCxPIAW&oh=00_AYC4r0GisXrvjN8Zt5q6Dnm1MAr-oU2_SG_CCwhfZZdx9w&oe=673B791F"
+                            src={profile}
                             alt="profile"
                         />
                     </div>
