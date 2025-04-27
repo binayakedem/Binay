67c4a55dbc7d872736ca4eef2de7ddb68dbac28b
binayakedem <binayakedem@gmail.com>
2025-04-27 12:57:59 +0545

    text-justify

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,95 +2,95 @@ import React from 'react'
 import banner from '../assets/limbu.jpg'
 import profile from '../assets/about.png'
 import { FaUniversity } from "react-icons/fa";
-import { FaLinkedin,FaFacebook,FaInstagram ,FaGithubSquare ,FaPhone} from "react-icons/fa";
+import { FaLinkedin, FaFacebook, FaInstagram, FaGithubSquare, FaPhone } from "react-icons/fa";
 import { MdOutlineAttachEmail } from "react-icons/md";
 const About = () => {
-    const email1 = 'binayalimbu300@gmail.com';
-    const email2= 'iambinayalimbu@gmail.com';
-    const handleEmailClick1 = () => {
-      window.open(`https://mail.google.com/mail/?view=cm&fs=1&to=${email1}`);
-    };
-    const handleEmailClick2 = () => {
-      window.open(`https://mail.google.com/mail/?view=cm&fs=1&to=${email2}`);
-    };
+  const email1 = 'binayalimbu300@gmail.com';
+  const email2 = 'iambinayalimbu@gmail.com';
+  const handleEmailClick1 = () => {
+    window.open(`https://mail.google.com/mail/?view=cm&fs=1&to=${email1}`);
+  };
+  const handleEmailClick2 = () => {
+    window.open(`https://mail.google.com/mail/?view=cm&fs=1&to=${email2}`);
+  };
   return (
     <>
-     <div className='h-60  w-full' data-aos="zoom-in"
-     data-aos-easing="ease-out-cubic"
-     data-aos-duration="2000">
+      <div className='h-60  w-full' data-aos="zoom-in"
+        data-aos-easing="ease-out-cubic"
+        data-aos-duration="2000">
         <img className='h-60 w-full object-cover' src={banner} alt="banner" />
 
-    </div>
-    <div className='bg-dark-50 text-light-gray lg:px-32 md:px-20 px-2'>
+      </div>
+      <div className='bg-dark-50 text-light-gray lg:px-32 md:px-20 px-2'>
         <div className='grid col-span-1 md:grid-cols-2 gap-5'>
-            <div className='overflow-hidden' data-aos="zoom-in" data-aos-duration="1200">
-                <img className='h-full w-auto' src={profile} alt="profile" />
+          <div className='overflow-hidden' data-aos="zoom-in" data-aos-duration="1200">
+            <img className='h-full w-auto' src={profile} alt="profile" />
+          </div>
+          <div className='p-6 my-2 ' data-aos="fade-up" data-aos-duration="2000">
+            <div>
+              <h1 className='text-black font-bold text-2xl my-6 font-mont'>About Me</h1>
+              <p className='text-gray-800 font-mono text-justify'>Hello! I'm an enthusiastic IT student who enjoys coding and continuous learning. My enthusiasm for technology motivates me to constantly adapt to new tools and frameworks. I thrive on solving complex problems and developing efficient, innovative solutions. With a strong interest in software development, I am committed to learning both frontend and backend technologies. Throughout my studies, I have completed over 20 projects that demonstrate my ability to apply theoretical knowledge to real-world scenarios. My journey is driven by curiosity and a desire to excel, with the goal of making a significant contribution to the tech industry. Let's connect and create something incredible together!</p>
             </div>
-            <div className='p-6 my-2 ' data-aos="fade-up" data-aos-duration="2000">
-                <div>
-                <h1 className='text-black font-bold text-2xl my-6 font-mont'>About Me</h1>
-                <p className='text-gray-800 font-mono '>Hello! I'm an enthusiastic IT student who enjoys coding and continuous learning. My enthusiasm for technology motivates me to constantly adapt to new tools and frameworks. I thrive on solving complex problems and developing efficient, innovative solutions. With a strong interest in software development, I am committed to learning both frontend and backend technologies. Throughout my studies, I have completed over 20 projects that demonstrate my ability to apply theoretical knowledge to real-world scenarios. My journey is driven by curiosity and a desire to excel, with the goal of making a significant contribution to the tech industry. Let's connect and create something incredible together!</p>
-                </div>
-                <div className='flex flex-col gap-3'>
-                    <h1 className='text-black font-bold text-2xl my-6 font-mont'>Education</h1>
-                    <div className='flex flex-row gap-2 items-center'>
-                    <FaUniversity color='black'/> <p className='text-gray-900'>Orchid International College,</p > <p className='text-warm-orange font-bold text-lg font-mono'>BSc.CSIT</p>
+            <div className='flex flex-col gap-3'>
+              <h1 className='text-black font-bold text-2xl my-6 font-mont'>Education</h1>
+              <div className='flex flex-row gap-2 items-center'>
+                <FaUniversity color='black' /> <p className='text-gray-900'>Orchid International College,</p > <p className='text-warm-orange font-bold text-lg font-mono'>BSc.CSIT</p>
 
-                    </div>
-                    <div className='flex flex-row gap-2 items-center'>
-                    <FaUniversity color='black'/> <p className='text-gray-900'>Nepal Mega College,</p > <p className='text-warm-orange font-bold text-lg font-mono'>+2 Science</p>
+              </div>
+              <div className='flex flex-row gap-2 items-center'>
+                <FaUniversity color='black' /> <p className='text-gray-900'>Nepal Mega College,</p > <p className='text-warm-orange font-bold text-lg font-mono'>+2 Science</p>
 
-                    </div>
-                    <div className='flex flex-row gap-2 items-center'>
-                    <FaUniversity color='black'/> <p className='text-gray-900'>Shree Sundevi Secondary School,</p > <p className='text-warm-orange font-bold text-lg font-mono'>SLC</p>
+              </div>
+              <div className='flex flex-row gap-2 items-center'>
+                <FaUniversity color='black' /> <p className='text-gray-900'>Shree Sundevi Secondary School,</p > <p className='text-warm-orange font-bold text-lg font-mono'>SLC</p>
 
-                    </div>
-                    
-                </div>
+              </div>
+
             </div>
+          </div>
 
         </div>
         <hr className='border-b-2 my-3' />
         <div className='grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8 lg:gap-12 h-96 pt-4'>
-                <iframe
-        src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d472.8095015057145!2d85.34084043398627!3d27.70040259506336!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2snp!4v1729261861189!5m2!1sen!2snp"
-        width="100%"
-        height="100%"
-        style={{ border: 0 }}
-        allowFullScreen
-        loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"
-      />
-      <div className='text-black flex flex-col gap-2    '>
-        <h1 className='font-bold text-lg text-yellow-600 font-mont'>Contacts</h1>
-        <div className='flex flex-row gap-2 items-center'>
-                <FaPhone color='green'/>
-                <p>+977-9843555471</p>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d472.8095015057145!2d85.34084043398627!3d27.70040259506336!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2snp!4v1729261861189!5m2!1sen!2snp"
+            width="100%"
+            height="100%"
+            style={{ border: 0 }}
+            allowFullScreen
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          />
+          <div className='text-black flex flex-col gap-2    '>
+            <h1 className='font-bold text-lg text-yellow-600 font-mont'>Contacts</h1>
+            <div className='flex flex-row gap-2 items-center'>
+              <FaPhone color='green' />
+              <p>+977-9843555471</p>
             </div>
             <div className='flex flex-row gap-2 items-center'>
-                <FaPhone color='green'/>
-                <p>+977-9803588079</p>
-            </div>
-        <div className='flex flex-row gap-2 items-center my-1 '>
-            <MdOutlineAttachEmail color='red' onClick={handleEmailClick2}/>
-                <p onClick={handleEmailClick2} className='cursor-pointer'>{email2}</p>
+              <FaPhone color='green' />
+              <p>+977-9803588079</p>
             </div>
             <div className='flex flex-row gap-2 items-center my-1 '>
-            <MdOutlineAttachEmail color='red' onClick={handleEmailClick1}/>
-                <p onClick={handleEmailClick1} className='cursor-pointer'>{email1}</p>
+              <MdOutlineAttachEmail color='red' onClick={handleEmailClick2} />
+              <p onClick={handleEmailClick2} className='cursor-pointer'>{email2}</p>
+            </div>
+            <div className='flex flex-row gap-2 items-center my-1 '>
+              <MdOutlineAttachEmail color='red' onClick={handleEmailClick1} />
+              <p onClick={handleEmailClick1} className='cursor-pointer'>{email1}</p>
             </div>
             <div className='flex flex-row gap-4 items-center '>
-                <p>Find me: </p>
-                 <a href="https://www.facebook.com/binayakdm" target='_blank'> <FaFacebook color='blue'/></a>
-                 <a href="https://github.com/binayakedem/" target='_blank'><FaGithubSquare color='black'/></a>
-                 <a href="https://www.instagram.com/binayalimbuz/" target='_blank'><FaInstagram color='red'/></a>
-                <a href="https://www.linkedin.com/in/binayalimbu/" target='_blank'> <FaLinkedin color='blue'/></a>
-               
+              <p>Find me: </p>
+              <a href="https://www.facebook.com/binayakdm" target='_blank'> <FaFacebook color='blue' /></a>
+              <a href="https://github.com/binayakedem/" target='_blank'><FaGithubSquare color='black' /></a>
+              <a href="https://www.instagram.com/binayalimbuz/" target='_blank'><FaInstagram color='red' /></a>
+              <a href="https://www.linkedin.com/in/binayalimbu/" target='_blank'> <FaLinkedin color='blue' /></a>
+
             </div>
+          </div>
+        </div>
+
       </div>
-      </div>
-                  
-    </div>
     </>
   )
 }
--- src/Pages/MyBlog.jsx
+++ src/Pages/MyBlog.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react"
 import blogPosts from "./data";
 const MyBlog = () => {
-    const [expandedPostIndex, setExpandedPostIndex] = useState(null);
+  const [expandedPostIndex, setExpandedPostIndex] = useState(null);
 
- 
+
   // Function to handle expanding/collapsing a post
   const toggleExpand = (index) => {
     setExpandedPostIndex(expandedPostIndex === index ? null : index);
@@ -21,8 +21,8 @@ const MyBlog = () => {
   };
   return (
     <div className='bg-dark-50 text-gray-900 lg:px-32 md:px-20 px-2'>
-        <h1 className="text-lg font-bold text-black text-center font-mont">Personal Experience Blog</h1>
-       {blogPosts.map((post, index) => (
+      <h1 className="text-lg font-bold text-black text-center font-mont">Personal Experience Blog</h1>
+      {blogPosts.map((post, index) => (
         <div
           key={index}
           className=" mx-auto my-4 p-4 bg-white shadow-md rounded-lg"
@@ -32,95 +32,95 @@ const MyBlog = () => {
           {expandedPostIndex === index ? (
             <div>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                 <div>
-                    <div className="overflow-hidden h-4/5"  data-aos="zoom-up" data-aos-duration="1000">
+                  <div className="overflow-hidden h-4/5" data-aos="zoom-up" data-aos-duration="1000">
                     <img className="h-full w-full object-cover rounded-md shadow  hover:scale-110 duration-500 ease-in-out cursor-pointer" src={post.primary_image} alt="primaryimage" />
+                  </div>
+                  <div className='flex flex-row items-center'>
+                    <div className='overflow-hidden h-10 w-10'>
+                      <img className='h-full w-full object-cover rounded-full ' src="https://www.binayalimbu.com.np/assets/about-Be7h1ghU.png" alt="profile" />
                     </div>
-                    <div className='flex flex-row items-center'>
-                <div className='overflow-hidden h-10 w-10'>
-                    <img className='h-full w-full object-cover rounded-full ' src="https://www.binayalimbu.com.np/assets/about-Be7h1ghU.png" alt="profile" />
+                    <div className='flex flex-row text-gray-700'>
+                      <p>By {post.author}</p>
+                      <p> || {post.update_time}</p>
+                    </div>
+                  </div>
                 </div>
-                <div className='flex flex-row text-gray-700'>
-                    <p>By {post.author}</p>
-                    <p> || {post.update_time}</p>
+                <div data-aos="fade-down"
+                  data-aos-easing="linear"
+                  data-aos-duration="1500">
+
+                  <h1 className="font-semibold text-lg my-2 text-black font-mont">Introduction</h1>
+                  <p className="text-justify" data-aos="zoom-up " data-aos-duration="1000">{post.main_paragraph}</p>
                 </div>
-            </div>
+                <div>
+                  <h1 className="font-semibold text-lg my-2 text-black font-mont">Background</h1>
+                  <p className="text-justify">{post.second_paragraph}</p>
                 </div>
-              <div data-aos="fade-down"
-     data-aos-easing="linear"
-     data-aos-duration="1500">
-                
-                <h1 className="font-semibold text-lg my-2 text-black font-mont">Introduction</h1>
-                <p data-aos="zoom-up" data-aos-duration="1000">{post.main_paragraph}</p>
+                <div className="overflow-hidden h-4/5" data-aos="zoom-in" data-aos-duration="1200">
+
+                  <img className="h-full w-full object-cover rounded-md hover:scale-110 duration-500 ease-in-out cursor-pointer" src={post.second_image} alt="primaryimage" />
+                </div>
               </div>
               <div>
-                <h1 className="font-semibold text-lg my-2 text-black font-mont">Background</h1>
-              <p>{post.second_paragraph}</p>
-              </div>
-              <div className="overflow-hidden h-4/5" data-aos="zoom-in" data-aos-duration="1200">
-                    
-                    <img className="h-full w-full object-cover rounded-md hover:scale-110 duration-500 ease-in-out cursor-pointer" src={post.second_image} alt="primaryimage" />
-                </div>
-                  </div>
-                  <div>
-            <h1 className="text-xl font-semibold mb-2 text-[#F39C12] text-center font-mont">Some Glimpse</h1>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4" data-aos="fade-down"
-     data-aos-easing="linear"
-     data-aos-duration="1500">
-                {post.images.map((image, idx) => (
+                <h1 className="text-xl font-semibold mb-2 text-[#F39C12] text-center font-mont">Some Glimpse</h1>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4" data-aos="fade-down"
+                  data-aos-easing="linear"
+                  data-aos-duration="1500">
+                  {post.images.map((image, idx) => (
                     <div key={idx} className="overflow-hidden h-60 cursor-pointer" onClick={() => openImageModal(image)}>
-                        <img 
-                            src={image}
-                            alt={`Image ${idx + 1}`}
-                            className="h-full w-full object-cover rounded-lg mb-2 hover:scale-125 duration-500 ease-in-out"
-                        />
+                      <img
+                        src={image}
+                        alt={`Image ${idx + 1}`}
+                        className="h-full w-full object-cover rounded-lg mb-2 hover:scale-125 duration-500 ease-in-out"
+                      />
                     </div>
-                ))}
-            </div>
+                  ))}
+                </div>
 
-            {/* Modal to display full-size image */}
-            {selectedImage && (
-    <div className="fixed inset-0 z-50 bg-black bg-opacity-75 flex items-center justify-center">
-        <div className="relative">
-            <img 
-                src={selectedImage} 
-                alt="Full size" 
-                className="w-screen h-screen   object-contain rounded-lg" 
-            />
-            <button
-                className="absolute top-4 left-1/2 transform -translate-x-1/2 text-white text-2xl font-bold bg-yellow-700 rounded-full px-6"
-                onClick={closeModal}
-            >
-                &times;
-            </button>
-        </div>
-    </div>
-)}
-        </div>              <button
+                {/* Modal to display full-size image */}
+                {selectedImage && (
+                  <div className="fixed inset-0 z-50 bg-black bg-opacity-75 flex items-center justify-center">
+                    <div className="relative">
+                      <img
+                        src={selectedImage}
+                        alt="Full size"
+                        className="w-screen h-screen   object-contain rounded-lg"
+                      />
+                      <button
+                        className="absolute top-4 left-1/2 transform -translate-x-1/2 text-white text-2xl font-bold bg-yellow-700 rounded-full px-6"
+                        onClick={closeModal}
+                      >
+                        &times;
+                      </button>
+                    </div>
+                  </div>
+                )}
+              </div>              <button
                 onClick={() => toggleExpand(index)}
                 className="mt-4 text-blue-500 text-start hover:text-blue-700 underline"
-                >
+              >
                 Close
               </button>
             </div>
           ) : (
             <div>
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-5">
-               <div>
-              <p data-aos="zoom-in-up" data-aos-duration="1200">{post.main_paragraph} ...</p>
-               </div>
-               <div className="overflow-hidden h-60"  data-aos="zoom-in" data-aos-duration="1200">
-                <img className="h-full w-full object-cover hover:scale-125 duration-500 ease-in-out cursor-grab" src={post.primary_image} alt="primary image" />
-               </div>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-5">
+                <div>
+                  <p className="text-justify" data-aos="zoom-in-up " data-aos-duration="1200">{post.main_paragraph} ...</p>
                 </div>
+                <div className="overflow-hidden h-60" data-aos="zoom-in" data-aos-duration="1200">
+                  <img className="h-full w-full object-cover hover:scale-125 duration-500 ease-in-out cursor-grab" src={post.primary_image} alt="primary image" />
+                </div>
+              </div>
               <button
                 onClick={() => toggleExpand(index)}
                 className="mt-4 text-blue-500 hover:text-blue-700 underline"
               >
                 Learn More
               </button>
-             
+
             </div>
           )}
         </div>
